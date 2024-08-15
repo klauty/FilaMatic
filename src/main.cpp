2e59582abcb92cf9cc8501c8b84af245d99e592e
klauty <kaue.developer@gmail.com>
2024-08-15 15:13:53 -0300

    implementing the stepper motor logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-#include "PID_utils.h"
+
 #include <PID_v1.h>
+#include <AccelStepper.h>
 
 #define POT_TEMP A7
 #define POT_VEL A6
@@ -36,8 +37,9 @@ Thermistor* thermistor;
 
 #define MOTOR_A1 6
 #define MOTOR_A2 7
-#define MOTOR_DIR 9
-#define MOTOR_STEP 8
+#define MOTOR_DIR 8
+#define MOTOR_STEP 9
+#define MOTOR_ENABLE 11
 
 #define MAX_TEMP 500
 
@@ -47,6 +49,7 @@ double targetTemp = 0;
 double temp = 0;
 int pwmVal = 0;
 int potMotor = 0;
+int motorVel = 0;
 int potTemp = 0;
 unsigned long deltaTela = 0;
 unsigned long deltaPID = 0;
@@ -59,8 +62,11 @@ double Setpoint, Input, Output;
 
 //Specify the links and initial tuning parameters
 //double Kp=1, Ki=0.05, Kd=0.25;
-double Kp=1, Ki=1, Kd=10;
+double Kp=1, Ki=0.01, Kd=0.25;
 PID myPID(&temp, &Output, &targetTemp, Kp, Ki, Kd, DIRECT);
+
+// Define a stepper and the pins it will use
+AccelStepper stepper(AccelStepper::DRIVER, MOTOR_STEP, MOTOR_DIR); // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
 
 void setup()
 {
@@ -78,39 +84,36 @@ void setup()
   lcd.init();
   lcd.setBacklight(1);
   Serial.begin(9600);
+  //stepper.setEnablePin(MOTOR_ENABLE);
+  //stepper.enableOutputs();
+  stepper.setMaxSpeed(800);
+  stepper.setSpeed(5);
+
   pinMode(BTN_MOTOR,INPUT_PULLUP);
   pinMode(BTN_TEMP,INPUT_PULLUP);
 
   pinMode(MOTOR_DIR,OUTPUT);
   pinMode(MOTOR_STEP,OUTPUT);
+  pinMode(MOTOR_ENABLE,OUTPUT);
+
+  //liga o motor -> active low
+  digitalWrite(MOTOR_ENABLE,LOW);
 
   pinMode(HEATER,OUTPUT);
   pinMode(LED_MOTOR,OUTPUT);
   pinMode(LED_TEMP,OUTPUT); 
 }
 
-int analogAverage(int pin,int reads){
+int analogAverage(int pin, int reads){
   int acc = 0;
-  for(int c =0; c< reads;c++){
+  for(int c =0; c < reads;c++){
     acc += analogRead(pin);
   }
   return acc/reads;
 }
 
-int getTemp(double read){
-  double temperature;
-  temperature = (SERIESRESISTOR / (1023 / read -1)) / THERMISTORNOMINAL;     // (R/Ro)
-  Serial.println(1023 / read );
-  temperature = log(temperature);                  // ln(R/Ro)
-  temperature /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-  temperature += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-  temperature = 1.0 / temperature;                 // Invert
-  temperature -= 273.15;                         // convert absolute temp to C
-  return temperature < 0 ? MAX_TEMP:temperature;
-}
-
 void updateScreen(){
-  if(millis() - deltaTela > 200){
+  if(millis() - deltaTela > 500){
     lcd.setCursor(0,0);
     lcd.print("T:");
     lcd.print(toggleHeater?"1:":"0:");
@@ -119,67 +122,73 @@ void updateScreen(){
     lcd.print(int(temp));
     lcd.print("  ");
     lcd.setCursor(0,1);                                                                                                                                
-    // lcd.print("M:");
-    // lcd.print(toggleMotor?"1:":"0:");
-    // lcd.print(map(analogAverage(POT_VEL,20),0,1024,50,270));
-    // lcd.print(" RPM");
-    // lcd.print("  ");
-    lcd.print(Output);
-    lcd.print(":");
-    lcd.print(PID_d);
+    lcd.print("M:");
+    lcd.print(toggleMotor?"1:":"0:");
+    lcd.print(motorVel);
+    lcd.print(" step/s ");
     lcd.print("  ");
+   // lcd.print(Output);
     deltaTela = millis();
 
   }
 }
 
-void loop()
-{
-  temp =  thermistor->readCelsius();//getTemp(analogAverage(THERMISTOR,1));
-  temp = temp < 0 ? MAX_TEMP:temp;
+void loop(){
+  // int temp_acc = 0;
+  // for(int c = 0; c<5 ;c++){
+  //   temp =  thermistor->readCelsius();
+  //   if(temp < 0){
+  //     c--;
+  //   }else{
+  //     temp_acc += temp;
+  //   }
+  // }
+  //temp = temp_acc/5;
+  //temp = temp < 0 ? MAX_TEMP:temp;
+
+  temp =  thermistor->readCelsius();
   targetTemp = map(analogAverage(POT_TEMP,1),0,1024,60,MAX_TEMP);
+  motorVel = map(analogAverage(POT_VEL,1),0,1024,16,800);
   updateScreen();
+  stepper.setSpeed(-motorVel);
  
   //verificando botao de ativacao do motor
   if(!digitalRead(BTN_MOTOR) && !toggleMotor){
     toggleMotor = true;
+    digitalWrite(MOTOR_ENABLE,LOW);
     delay(500);
   }
   if(!digitalRead(BTN_MOTOR) && toggleMotor){
     toggleMotor = false;
+    digitalWrite(MOTOR_ENABLE,HIGH);
     delay(500);
   }
 
   //verificando botao de ativacao do aquecedor
   if(!digitalRead(BTN_TEMP) && !toggleHeater){
     toggleHeater = true;
-    //analogWrite(LED_TEMP,255);
     delay(500);
   }
   if(!digitalRead(BTN_TEMP) && toggleHeater){
     toggleHeater = false;
-    //analogWrite(LED_TEMP,0);
     delay(500);
   }
 
   if(toggleHeater){
-    //implementar a logica do PID aqui
-    if(millis() - deltaPID > 500){
-      //pwmVal = getHeaterPower(temp,targetTemp);
-      //analogWrite(HEATER,pwmVal);
-      deltaPID = millis();
-    }
-    //analogWrite(HEATER,map(analogRead(POT_TEMP),0,1024,0,255));
     myPID.Compute();
-    analogWrite(HEATER, Output/2);
+    analogWrite(HEATER,Output);
   }else{
     analogWrite(HEATER,0);
-    pwmVal = 0;
   }
 
   if(toggleMotor){
     // ligar motor de passo
+    stepper.runSpeed();
+    //stepper.runToNewPosition(0);
+    //stepper.runToNewPosition(500);
+    //stepper.runToNewPosition(100);
+    //stepper.runToNewPosition(120);
   }else{
-    //desligar motor
+    //stepper.disableOutputs();
   }
 }
